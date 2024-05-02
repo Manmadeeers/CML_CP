1d80d909661b2c072a8b6c162be59352d4c4ae44
Manmad <philmade6@gmail.com>
2024-05-02 17:55:23 +0300

    xml and js fixes. some files deleted

--- buttons.js
+++ buttons.js
@@ -1,24 +1,20 @@
+//functions for buttons
 function regbutton() {
      window.location.href = "registration.html";
-}
-
-function wearlink() {
-     window.location.href = "wear.html";
-}
-
-function shoelink() {
-     window.location.href = "shoe.html";
-}
-function equiplink() {
-     window.location.href = "equipment.html";
 }
 function goodlink() {
      window.location.href = "error.html";
 }
+function sells(path) {
+     window.location.href = `goods.html?path=${path}`;
+}
+// end
 
+// opens mobile menu
 document.getElementById('menu-button').addEventListener('click', function () {
      document.getElementById('mobile-menu').classList.toggle('open');
 });
 document.getElementById('close-menu').addEventListener('click',function(){
      document.getElementById('mobile-menu').classList.remove('open');
 })
+// end
--- display-data.js
+++ display-data.js
@@ -1,11 +1,53 @@
-function getID(id){
-    if(id=="wear"){
-
-    }
-    else if(id=="shoe"){
-
-    }
-    else if(id=="equip"){
-        
-    }
+let goods = [];
+let WAY = "";
+function loadXML(url) {
+    return fetch(url)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            goods = Array.from(data.querySelectorAll('good')).map(goods => {
+                return {
+                    name: goods.querySelector('name').textContent,
+                    price: goods.querySelector('price').textContent,
+                    image: goods.querySelector('image').textContent,
+                };
+            });
+            return goods;
+        });
 }
+function addItemsToDOM(goods) {
+    const goodsList = document.querySelector('.goods');
+    // Очищаем список перед добавлением новых элементов
+    // goodsList.innerHTML = '';
+    goods.forEach(good => {
+        console.log(good);
+        const goodDiv = document.createElement('div');
+        goodDiv.className = 'good';
+        goodDiv.innerHTML = `
+            <img src="${good.image}">
+            <p>${good.name}</p>
+            <p> ${good.price}</p>
+        `;
+        // Добавляем обработчик событий onclick
+        goodDiv.onclick = function() {
+            loadItemDetails(WAY, good.name);
+        };
+        goodsList.appendChild(goodDiv);
+    });
+}
+function getID(){
+    const params = new URLSearchParams(window.location.search);
+    const pathURL =params.get('path');
+    WAY = "data/"+pathURL+".xml";
+    console.log(WAY);
+    return WAY;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let got = getID();
+    loadXML(got).then(() => {
+    console.log(goods.name); 
+    addItemsToDOM(goods);
+    })
+    .catch(error => console.error('Error fetching XML:', error));
+})
