0705a381608fbe1f969be5efd24af1027eed5e75
Manmad <philmade6@gmail.com>
2024-05-02 18:46:33 +0300

    js fixes

--- display-data.js
+++ display-data.js
@@ -20,7 +20,7 @@ function addItemsToDOM(goods) {
     // Очищаем список перед добавлением новых элементов
     // goodsList.innerHTML = '';
     goods.forEach(good => {
-        console.log(good);
+        // console.log(good);
         const goodDiv = document.createElement('div');
         goodDiv.className = 'good';
         goodDiv.innerHTML = `
@@ -29,25 +29,57 @@ function addItemsToDOM(goods) {
             <p> ${good.price}</p>
         `;
         // Добавляем обработчик событий onclick
-        goodDiv.onclick = function() {
+        goodDiv.onclick = function () {
             loadItemDetails(WAY, good.name);
         };
         goodsList.appendChild(goodDiv);
     });
 }
-function getID(){
+function getID() {
     const params = new URLSearchParams(window.location.search);
-    const pathURL =params.get('path');
-    WAY = "data/"+pathURL+".xml";
+    const pathURL = params.get('path');
+    WAY = "data/" + pathURL + ".xml";
     console.log(WAY);
     return WAY;
 }
+function setWay(currentPath) {
+    let name;
+    if (currentPath === "data/data.xml") {
+        name = "Одежда";
+    }
+    else if (currentPath === "data/shoe.xml") {
+        name = "Обувь";
+    }
+    else if (currentPath === "data/equip.xml") {
+        name = "Экипировка";
+    }
+    return name;
+}
+function getButton(currentPath){
+    let id;
+    if (currentPath === "data/data.xml") {
+        id = "wear-button";
+    }
+    else if (currentPath === "data/shoe.xml") {
+        id = "shoe-button";
+    }
+    else if (currentPath === "data/equip.xml") {
+        id = "equip-button";
+    }
+    return id;
+}
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+
     let got = getID();
+    let NAME = setWay(got);
+    let changeBG = getButton(got);
     loadXML(got).then(() => {
-    console.log(goods.name); 
-    addItemsToDOM(goods);
+        document.getElementById('good-name').textContent = NAME;
+        document.getElementById(changeBG).style.backgroundColor = "#171A5B";
+        document.getElementById(changeBG).style.color = "white";
+        console.log(goods.name);
+        addItemsToDOM(goods);
     })
-    .catch(error => console.error('Error fetching XML:', error));
+        .catch(error => console.error('Error fetching XML:', error));
 })
